49bd53d35184f31576cff23887b225f3399e5fc5
FIlip Dorian Pindej <fpindej@users.noreply.github.com>
2026-02-01 22:02:54 +0100

    chore: configure project (slug: recipes, ports: 20000/20002/20004/20006/20008)

--- src/frontend/src/lib/config/server.ts
+++ src/frontend/src/lib/config/server.ts
@@ -1,5 +1,5 @@
 import { env } from '$env/dynamic/private';
 
 export const SERVER_CONFIG = {
-	API_URL: env.API_URL || 'http://localhost:{INIT_API_PORT}'
+	API_URL: env.API_URL || 'http://localhost:20002'
 };
